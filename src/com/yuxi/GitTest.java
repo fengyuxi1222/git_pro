0e4599eab878a4e2afa0270580a6bd5993ca9d2b
feng <389967058@qq.com>
2019-11-26 18:32:54 +0800

    第一次更改

--- src/com/yuxi/GitTest.java
+++ src/com/yuxi/GitTest.java
@@ -2,6 +2,6 @@ package com.yuxi;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello git!");
+        System.out.println("Hello Git!");
     }
 }
